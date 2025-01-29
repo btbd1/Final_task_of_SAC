57bbfeb497b2b01195b0e98205105ac384d2bb7f
btbd1 <annchest1@gmail.com>
2025-01-29 16:46:05 +0400

    add tests for basket check

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
-# import time
+import time
 import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -48,12 +50,14 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_item_to_basket()
     page.should_success_message_disappear()
 
+
 @pytest.mark.login_link
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 @pytest.mark.login_link
 def test_guest_can_go_to_login_page_from_product_page(browser):
@@ -63,3 +67,24 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+@pytest.mark.check_is_basket_empty
+def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_items_in_basket()
+
+
+@pytest.mark.check_is_basket_empty
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_message_in_empty_basket()
